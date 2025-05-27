bd92c130b96e42dcff0b94a12bd1218176a60c84
charnelledev <kengnemaiva54@gmail.com>
2025-05-27 14:03:27 +0100

    first commit

--- index.php
+++ index.php
@@ -45,7 +45,6 @@ $events = $pdo->query("SELECT * FROM events WHERE statut = 'actif' ORDER BY date
     <!-- Conférences professionnelles -->
 
 
-
     <div class="bg-white shadow-lg rounded-2xl overflow-hidden hover:shadow-xl  transform transition-all duration-500 hover:scale-110">
       <div class="overflow-hidden">
         <img src="assets/images/Corporate Events Services.jpg" alt="Conférences" class="w-full h-52 object-cover transform transition-all duration-500 hover:scale-110 hover:blur-sm">
@@ -114,66 +113,6 @@ $events = $pdo->query("SELECT * FROM events WHERE statut = 'actif' ORDER BY date
   </div>
 </section>
 
-
-
-
-  
-    <!-- Conférences professionnelles -->
-    <!-- <div class="bg-white shadow-lg rounded-2xl overflow-hidden hover:shadow-xl transition">
-      <img src="assets/images/Corporate Events Services.jpg" alt="Conférences" class="w-full h-52 object-cover">
-      <div class="p-6">
-        <h2 class="text-2xl font-semibold text-indigo-700">Conférences professionnelles</h2>
-        <p class="mt-2 text-gray-600">Participez à des conférences métiers, panels d'experts et discussions sur les dernières innovations.</p>
-      </div>
-    </div> -->
-
-    <!-- Concerts ou festivals -->
-    <!-- <div class="bg-white shadow-lg rounded-2xl overflow-hidden hover:shadow-xl transition">
-      <img src="assets/images/téléchargement (68).jpg" alt="Concerts" class="w-full h-52 object-cover">
-      <div class="p-6">
-        <h2 class="text-2xl font-semibold text-indigo-700">Concerts & Festivals</h2>
-        <p class="mt-2 text-gray-600">Vibrez au rythme des plus grands artistes locaux et internationaux lors de festivals inoubliables.</p>
-      </div>
-    </div> -->
-
-    <!-- Webinaires et séminaires -->
-    <!-- <div class="bg-white shadow-lg rounded-2xl overflow-hidden hover:shadow-xl transition">
-      <img src="assets/images/Dans notre dernier article, nous abordons comment….jpg" alt="Webinaires" class="w-full h-52 object-cover">
-      <div class="p-6">
-        <h2 class="text-2xl font-semibold text-indigo-700">Webinaires & Séminaires</h2>
-        <p class="mt-2 text-gray-600">Rejoignez nos séminaires en ligne depuis chez vous pour apprendre, échanger et progresser.</p>
-      </div>
-    </div> -->
-
-    <!-- Salons / Foires -->
-    <!-- <div class="bg-white shadow-lg rounded-2xl overflow-hidden hover:shadow-xl transition">
-      <img src="assets/images/f6a7b843-341a-4679-833a-5c8174131f38.jpg" alt="Salon" class="w-full h-52 object-cover">
-      <div class="p-6">
-        <h2 class="text-2xl font-semibold text-indigo-700">Salons & Foires</h2>
-        <p class="mt-2 text-gray-600">Explorez nos salons culturels, technologiques et foires commerciales dans plusieurs régions.</p>
-      </div>
-    </div> -->
-
-    <!-- Événements associatifs ou scolaires -->
-    <!-- <div class="bg-white shadow-lg rounded-2xl overflow-hidden hover:shadow-xl transition">
-      <img src="assets/images/A student of Glenmuir High School speaks at a….jpg" alt="Événement associatif" class="w-full h-52 object-cover">
-      <div class="p-6">
-        <h2 class="text-2xl font-semibold text-indigo-700">Événements associatifs & scolaires</h2>
-        <p class="mt-2 text-gray-600">Engagez-vous dans des événements communautaires, journées culturelles, activités scolaires.</p>
-      </div>
-    </div> -->
-
-    <!-- Formations ou ateliers -->
-    <!-- <div class="bg-white shadow-lg rounded-2xl overflow-hidden hover:shadow-xl transition">
-      <img src="assets/images/Team building artistique _ création d'une fresque….jpg" alt="Formation" class="w-full h-52 object-cover image-hover-zoom">
-      <div class="p-6">
-        <h2 class="text-2xl font-semibold text-indigo-700">Formations & Ateliers</h2>
-        <p class="mt-2 text-gray-600">Améliorez vos compétences grâce à nos sessions pratiques, ateliers techniques et formations intensives.</p>
-      </div>
-    </div>
-
-  </div>
-</section> -->
 
 <?php require_once 'includes/footer.php'; ?>
 
